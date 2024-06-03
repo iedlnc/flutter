8696666f0ba9355699ed2012ec9481065d919c98
iedlnc <lee126984573@gmail.com>
2024-06-03 16:10:20 +0900

    asas

--- webtoon_test/lib/screens/detail_screen.dart
+++ webtoon_test/lib/screens/detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
 //import 'package:url_launcher/url_launcher.dart';
 //import 'package:url_launcher/url_launcher_string.dart';
 import 'package:webtoon_test/models/webtoon_detail_model.dart';
